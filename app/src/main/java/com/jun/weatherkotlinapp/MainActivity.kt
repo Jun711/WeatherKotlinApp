2065dad0a96d19ff2fd0e5c10923669df8105ac7
--get-all <cygoh@umich.edu>
2017-06-15 00:37:51 -0700

    Build GoogleAPIClient

--- app/src/main/java/com/jun/weatherkotlinapp/MainActivity.kt
+++ app/src/main/java/com/jun/weatherkotlinapp/MainActivity.kt
@@ -4,10 +4,22 @@ import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener  {
+    // Const
+    val PERMISSION_REQUEST_CODE = 1001
+    val PLAY_SERVICE_RESOLUTION_REQUEST = 1000
 
-    val  PERMISSION_REQUEST_CODE = 1001
+    // Variable
+    var mGoogleApiClient: GoogleApiClient? = null
+    var mLocationRequst: LocationRequest? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun requestPermission() {
 //        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                         != PackageManager.PERMISSION_GRANTED)
@@ -29,7 +41,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayService()) {
+                        buildGoogleApiClient()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun buildGoogleApiClient() {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build()
+    }
+
+    private fun checkPlayService(): Boolean {
+        var resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICE_RESOLUTION_REQUEST).show()
+            } else {
+                Toast.makeText(applicationContext, "This device is not supported", Toast.LENGTH_SHORT).show()
+                // finish()
+                // Call this when your activity is done and should be closed.
+                finish()
+            }
+            return false
+        }
+        return true
     }
 
 
